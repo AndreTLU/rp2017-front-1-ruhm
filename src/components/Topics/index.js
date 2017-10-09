edf62f7bfcc320deda2f5b29ec9777e6904892fe
Kardo <kardoj@gmail.com>
2017-10-09 16:57:56 +0300

    Redux

--- src/components/Topics/index.js
+++ src/components/Topics/index.js
@@ -1,56 +1,9 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Api from '../../utils/Api'
-import List from './List'
+import { connect } from 'react-redux'
+import { getTopics } from './TopicsActions'
+import Topics from './Topics'
 
-import Form from './Form'
+const mapStateToProps = state => ({
+  topics: state.topics
+})
 
-class Topics extends React.Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      topics: []
-    }
-
-    this.getTopics = this.getTopics.bind(this)
-  }
-
-  componentDidMount () {
-    console.log('load topics')
-    this.getTopics()
-  }
-
-  getTopics () {
-    Api('GET', '/topics', {})
-    .then(results => {
-      const { topics } = results
-      console.log(topics)
-
-      this.setState({ topics })
-    })
-    .catch(error => {
-      console.error(error)
-    })
-  }
-
-  render () {
-    console.log('RENDER TOPICS')
-    const { topics } = this.state
-
-    return (
-      <div id='topics'>
-        <h1>Topics</h1>
-        <Link to='/'>Home</Link>
-        <br />
-        <br />
-        <Form getTopics={this.getTopics} />
-        <br />
-        <br />
-        <List topics={topics} />
-      </div>
-    )
-  }
-}
-
-export default Topics
+export default connect(mapStateToProps, { getTopics })(Topics)
